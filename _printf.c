591561bc0f28ac21a294cd2a5b21a840719a3109
Frankley Omwenga <nchorefrankley27@gmail.com>
2022-07-12 14:54:41 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -9,36 +9,46 @@
 
 int _printf(const char *format, ...)
 {
-	int printed_chars;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	convert_t f_list[BUFFSIZE] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_decimal},
-		{"i", print_integer},
-		{"x", _hex_l},
-		{"X", _hex_u},
-		{"p", _hex_l},
-		{"S", print_string},
-		{"r", print_r},
-		{"R", rot_13},
-		{"b", print_binary},
-		{"u", print_unint},
-		{"o", print_octal},
-		{NULL, NULL},
-	};
-	va_list arg_list;
-
-	if (format == NULL)
-	{
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{
+				function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			}++i;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-
-	va_start(arg_list, format);
-	/*call functions */
-	printed_chars = parser(format, f_list, arg_list);
-
-	va_end(arg_list);
-	return (printed_chars);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
